b7cdff81b3a34d34d92100484bbdeadd8b82b400
Asliddin Azizovich <space.coding.programmer@gmail.com>
2021-10-16 23:44:44 +0500

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Asliddin Abdivasiyev on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
